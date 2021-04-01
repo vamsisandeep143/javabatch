58103e0c862d7b5f23339d27a9efb16d1673974a
sandeep <vamsisandeep.sundara@gmail.com>
2021-04-02 01:25:52 +0530

    initial import

---
+++ TypeCasting/src/Test.java
@@ -0,0 +1,18 @@
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	/*char x = '=';
+	int  y = 2;*/
+		
+		float x =  9.5f;
+		int sal =(int) x;
+      //  float sal = (float)x; 
+	
+	//y = (int) x;
+	System.out.println(x);
+
+	}
+
+}
